652ce4ba6e1f3d8a1a16ff40e3f5055679515361
VargaAti <varga.attila@diak.szbi-pg.hu>
2026-02-25 08:58:57 +0100

    finish main

--- forma1.py
+++ forma1.py
@@ -12,11 +12,51 @@ Olvasd be az f1.txt adatait, majd oldd meg az alábbi feladatokat!
 
 A megoldott feladatokat a kiirt_adatok nevű mappába hozd létre statisztika.txt néven!
 """
+import statistics as st
+
+versenyzok = []
+with open('beolvasando_adatok/f1.txt', 'r', encoding='utf-8') as forrasfajl:
+    next(forrasfajl)
+    for sor in forrasfajl:
+        adatok = sor.strip().split(';')
+        nev = adatok[0]
+        csapat = adatok[1]
+        gyozelem = int(adatok[2])
+        futam = int(adatok[3])
+        versenyzo = {'nev': nev, 'csapat': csapat, 'gyozelem': gyozelem, 'futam': futam}
+        versenyzok.append(versenyzo)
+
+print(f'{versenyzok=}')
+
+#1. feladat
+versenyzok_szama = 0
+for i in versenyzok:
+    if i["nev"] != "":
+        versenyzok_szama += 1
+
+#2. feladat
+gyozelmek_listaja = []
+legtobb_gyozelem = versenyzok[0]
+for i in versenyzok:
+    gyozelmek_listaja.append(i["gyozelem"])
+legtobb_gyozelem = versenyzok[gyozelmek_listaja.index(max(gyozelmek_listaja))]
+
+#3. feladat
+legkevesebb_gyozelem = versenyzok[gyozelmek_listaja.index(min(gyozelmek_listaja))]
+
+#4. feladat
+futamok_listaja = []
+legtobb_futam = versenyzok[0]
+for i in versenyzok:
+    futamok_listaja.append(i["futam"])
+legtobb_futam = versenyzok[futamok_listaja.index(max(futamok_listaja))]
+
+#6. feladat
 
 
-print("1. A beolvasott fájlban összesen ____ versenyző szerepel.")
-print("2. A legtöbb futamot nyert versenyző: ____")
-print("3. A legkevesebb futamot nyert versenyző: ____")
-print("4. A legtöbb futamot teljesített versenyző: ____")
-print("5. Az átlagos futamszám: ____")
-print("***A legtöbb futamgyőzelmet szerző csapat: ____")
+print(f"1. A beolvasott fájlban összesen {versenyzok_szama} versenyző szerepel.")
+print(f"2. A legtöbb futamot nyert versenyző: {legtobb_gyozelem["nev"]}")
+print(f"3. A legkevesebb futamot nyert versenyző: {legkevesebb_gyozelem["nev"]}")
+print(f"4. A legtöbb futamot teljesített versenyző: {legtobb_futam["nev"]}")
+print(f"5. Az átlagos futamszám: {round(st.mean(i["futam"] for i in versenyzok), 2)}")
+print(f"***A legtöbb futamgyőzelmet szerző csapat: ____")
